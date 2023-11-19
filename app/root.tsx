4382d4b6ca9aff3f63b23a70aa8d301af0e4b320
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2023-11-19 21:27:23 +0700

    Init tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,17 @@
 import {
-  Form,
   Links,
   LiveReload,
   Meta,
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import Layout from "./layout";
+import stylesheet from "~/tailwind.css";
+import { type LinksFunction } from "@remix-run/node";
 
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
 export default function App() {
   return (
     <html lang="en">
@@ -17,35 +22,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-
+        <Layout />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
