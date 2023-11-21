671c2addecbcdbdb795d61cb607ac83892a72fa7
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2023-11-22 01:54:46 +0700

    show answer

--- app/components/Content/index.tsx
+++ app/components/Content/index.tsx
@@ -1,27 +1,28 @@
 import dayjs from "~/config/dayjs";
 import { Avatar } from "..";
 import { Typography } from "@mui/material";
-import { type QuestionComponentType } from ".";
+import { type QuestionComponentType } from "../Question";
+import { type AnswerComponentType } from "../Answer";
 
 interface ContentProps {
-  question: QuestionComponentType;
+  content: QuestionComponentType | AnswerComponentType;
 }
 
-const Content: React.FC<ContentProps> = ({ question }) => {
+const Content: React.FC<ContentProps> = ({ content }) => {
   return (
     <div className="flex flex-col gap-2">
       <div className="flex flex-row gap-1 ">
-        <Avatar user={question.user} />
+        <Avatar user={content.user} />
         <div className="flex flex-col">
           <Typography sx={{ fontSize: 12, fontWeight: 600 }}>
-            {question.user.name}
+            {content.user.name}
           </Typography>
           <Typography sx={{ fontSize: 10 }}>
-            {dayjs(new Date(question.createdAt || "")).fromNow()}
+            {dayjs(new Date(content.createdAt || "")).fromNow()}
           </Typography>
         </div>
       </div>
-      <Typography sx={{ fontSize: 14 }}>{question.content}</Typography>
+      <Typography sx={{ fontSize: 14 }}>{content.content}</Typography>
     </div>
   );
 };
--- app/components/Question/index.tsx
+++ app/components/Question/index.tsx
@@ -1,8 +1,10 @@
 import { Prisma } from "@prisma/client";
 import { type SerializeFrom } from "@remix-run/node";
-import Content from "./Content";
+import Content from "../Content";
 import AnswerEditor from "./AddAnswer";
 import { EditorType } from "../Editor";
+import Answers from "./Answers";
+import { answerSelect } from "../Answer";
 
 export const questionSelect: Prisma.QuestionSelect = {
   id: true,
@@ -14,6 +16,7 @@ export const questionSelect: Prisma.QuestionSelect = {
       avatarId: true,
     },
   },
+  answers: { select: answerSelect },
   createdAt: true,
 };
 
@@ -29,8 +32,9 @@ const Question = ({ question }: { question: QuestionComponentType }) => {
   if (!question) return <></>;
   return (
     <div className="flex flex-col w-full p-4 gap-2 border-b-[1px] border-gray-300">
-      <Content question={question} />
+      <Content content={question} />
       <AnswerEditor parentId={question.id} editorType={EditorType.new} />
+      <Answers answers={question.answers} />
     </div>
   );
 };
